c6684eb4174c00656c4cbf972693acca77eac0ee
Lorenzo Addazi <lorenzo.addazi@rcp-vision.com>
2019-02-14 19:04:43 +0100

    guessing_game - first

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please insert your guess...");
+    // mutable string variable
+    let mut guess = String::new();
+    // read input
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line...");
+    // print guess
+    println!("You guessed: {}", guess);
 }
