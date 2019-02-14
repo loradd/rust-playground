dda4537d0e808743f75ee9ab914c1ea8011e3c2b
Lorenzo Addazi <loradd@Lorenzos-MBP.station>
2019-02-14 20:19:00 +0100

    guessing_game - removed Cargo.lock from .gitignore file

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,11 @@
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
     println!("Please insert your guess...");
     // mutable string variable
     let mut guess = String::new();
     // read input
-    io::stdin()
-        .read_line(&mut guess)
+    io::stdin().read_line(&mut guess)
         .expect("Failed to read line...");
     // print guess
     println!("You guessed: {}", guess);
