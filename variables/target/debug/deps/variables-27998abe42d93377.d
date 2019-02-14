ce296981568378c498eb2093f9764e601eb5def7
Lorenzo Addazi <lorenzo.addazi@rcp-vision.com>
2019-02-14 21:07:33 +0100

    removes target files and aligns .gitignore

--- variables/target/debug/deps/variables-27998abe42d93377.d
+++
@@ -1,5 +0,0 @@
-/Users/loradd/Documents/Development/rust-playground/variables/target/debug/deps/variables-27998abe42d93377: src/main.rs
-
-/Users/loradd/Documents/Development/rust-playground/variables/target/debug/deps/variables-27998abe42d93377.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/deps/variables-c5b53bc44dff862c.d
+++
@@ -1,5 +0,0 @@
-/Users/loradd/Documents/Development/rust-playground/variables/target/debug/deps/variables-c5b53bc44dff862c.rmeta: src/main.rs
-
-/Users/loradd/Documents/Development/rust-playground/variables/target/debug/deps/variables-c5b53bc44dff862c.d: src/main.rs
-
-src/main.rs:
