df6933741111f2f6e45a7a109bc0f3505383ac06
Lorenzo Addazi <lorenzo.addazi@rcp-vision.com>
2019-02-17 19:03:31 +0100

    fibonacci / the twelve days of christmas

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -10,21 +10,19 @@ fn main() {
         Ok(sequence_index) => println!(
             "Fibonacci[{}]: {}",
             sequence_index,
-            fibonacci_iterative_naive(sequence_index)
+            fibonacci(sequence_index)
         ),
         Err(_) => println!("Illegal Fibonacci sequence index"),
     };
 }
 
-fn fibonacci_iterative_naive(n: u8) -> u128 {
+fn fibonacci(n: u8) -> u128 {
     match n {
         0 => 0,
-        1 => 1,
+        1 | 2 => 1,
         _ => {
-            let mut f_n: u128 = 0;
-            let mut f_n_1: u128 = 1;
-            let mut f_n_2: u128 = 0;
-            for _ in 1..n {
+            let (mut f_n, mut f_n_1, mut f_n_2) = (0, 1, 1);
+            for _ in 2..n {
                 f_n = f_n_1 + f_n_2;
                 f_n_2 = f_n_1;
                 f_n_1 = f_n;
@@ -34,38 +32,34 @@ fn fibonacci_iterative_naive(n: u8) -> u128 {
     }
 }
 
-#[allow(dead_code)]
-fn fibonacci_recursive_naive(n: &u8) -> u128 {
-    match n {
-        0 => 0,
-        1 | 2 => 1,
-        _ => fibonacci_recursive_naive(&(n - 1)) + fibonacci_recursive_naive(&(n - 2)),
-    }
-}
-
 #[cfg(test)]
 mod tests {
 
-    use super::fibonacci_iterative_naive;
+    use super::fibonacci;
 
     #[test]
-    fn zero_value() {
-        assert_eq!(0, fibonacci_iterative_naive(0));
+    fn f_0() {
+        assert_eq!(0, fibonacci(0));
     }
 
     #[test]
-    fn one_value() {
-        assert_eq!(1, fibonacci_iterative_naive(1));
-        assert_eq!(1, fibonacci_iterative_naive(2));
+    fn f_1() {
+        assert_eq!(1, fibonacci(1));
     }
 
     #[test]
-    fn greater_value() {
-        assert_eq!(354224848179261915075, fibonacci_iterative_naive(100));
-        assert_eq!(
-            1226132595394188293000174702095995,
-            fibonacci_iterative_naive(160)
-        );
+    fn f_2() {
+        assert_eq!(1, fibonacci(2));
+    }
+
+    #[test]
+    fn f_100() {
+        assert_eq!(354224848179261915075, fibonacci(100));
+    }
+
+    #[test]
+    fn f_160() {
+        assert_eq!(1226132595394188293000174702095995, fibonacci(160));
     }
 
 }
